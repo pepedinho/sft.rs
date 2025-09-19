bcd7f8862f67ac709ff9625acd96f22418fbcbcc
pepedinho <2spii94@gmail.com>
2025-09-19 21:49:18 +0200

    proto: Start implementation of SFT protocol

--- src/bin/sft.rs
+++ src/bin/sft.rs
@@ -1,8 +1,25 @@
-use clap::Parser;
-use sft::client::cli::{Cli, PackageInfos};
+use std::time::Duration;
 
-fn main() -> anyhow::Result<()> {
+use clap::Parser;
+use sft::{
+    client::cli::{Cli, PackageInfos},
+    protocol::SFT,
+};
+use tokio::{net::TcpStream, time::timeout};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let pckginfos = PackageInfos::parse_command(Cli::parse().command)?;
     println!("{:#?}", pckginfos);
+
+    let mut stream = timeout(
+        Duration::from_secs(5),
+        TcpStream::connect(pckginfos.host + ":8000"),
+    )
+    .await??;
+
+    let r = SFT::auth(&mut stream).await?;
+
+    println!("{r}");
     Ok(())
 }
