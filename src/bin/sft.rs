751fc9d21912909aac88cad830915b292959bcf2
pepedinho <2spii94@gmail.com>
2025-09-19 19:45:21 +0200

    cli: implement clap parsing of cli arguments

--- src/bin/sft.rs
+++ src/bin/sft.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("oui je la");
+use clap::Parser;
+use sft::client::cli::{Cli, PackageInfos};
+
+fn main() -> anyhow::Result<()> {
+    let pckginfos = PackageInfos::parse_command(Cli::parse().command)?;
+    println!("{:#?}", pckginfos);
+    Ok(())
 }
